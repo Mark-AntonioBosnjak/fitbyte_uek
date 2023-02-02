02ecd0aaa6dbe0d54d3a3d6b8c28720f05e1b14c
Mark-Antonio_Bosnjak <mab151204@gmail.com>
2023-02-02 06:58:59 +0100

    Added Menu for Changing Activities

--- app/src/main/java/com/netcetera/fitbyte_uek/Activity2.java
+++ app/src/main/java/com/netcetera/fitbyte_uek/Activity2.java
@@ -9,6 +9,7 @@ public class Activity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity1);
     }
 }
